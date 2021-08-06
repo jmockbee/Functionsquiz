9ab18f815cebe1040b47a4cca3bb2eaca0c24a28
Jon Mockbee <mockbee1000@gmail.com>
2021-08-05 21:39:07 -0400

    writing last function

--- functionquiz.py
+++ functionquiz.py
@@ -15,3 +15,13 @@ amount_a = get_seconds(2,30,0)
 amount_b = get_seconds(0,45,15)
 result = amount_a+amount_b
 print("This many seconds: " +str(result))
+
+
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes *60
+    return hours,minutes,remaining_seconds 
+
+hours, minutes, seconds = convert_seconds (5000)
+print (hours, minutes, seconds)
