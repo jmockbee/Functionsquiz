d6e3cd74c9d654a87e8a40dc13dd592cc499efb6
Jon Mockbee <mockbee1000@gmail.com>
2021-08-05 21:25:08 -0400

    more changes

--- functionquiz.py
+++ functionquiz.py
@@ -6,3 +6,12 @@ area_b = area_triangle (7,3)
 
 sum = area_a + area_b
 print ("the sum of both areas is:  " + str(sum))
+
+
+def get_seconds(hours, minutes, seconds):
+  return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2,30,0)
+amount_b = get_seconds(0,45,15)
+result = amount_a+amount_b
+print("This many seconds: " +str(result))
